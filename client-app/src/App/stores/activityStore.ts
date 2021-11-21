a6465dd15f228e62f77ae6c916d5046b53236393
Yuliia <julia.myshchenko@gmail.com>
2021-11-21 14:47:41 +0200

    End of section 9

--- client-app/src/App/stores/activityStore.ts
+++ client-app/src/App/stores/activityStore.ts
@@ -1,7 +1,6 @@
 import { makeAutoObservable, runInAction } from "mobx";
 import { Activity } from "../models/activity";
 import agent2 from '../api/agent2';
-import { textChangeRangeIsUnchanged } from "typescript";
 
 export default class ActivityStore{
     activityRegistry = new Map<string, Activity>();
@@ -17,6 +16,16 @@ export default class ActivityStore{
     get activitiesByDate(){
         return Array.from(this.activityRegistry.values()).sort((a, b) => 
                 Date.parse(a.date) - Date.parse(b.date));
+    }
+
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity ) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity]
+                return activities;
+            }, {} as {[key: string]: Activity[]})
+        )
     }
 
     loadActivities = async () => {
